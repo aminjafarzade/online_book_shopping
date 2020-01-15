8ebaefe8886ebbd116119695e95ae62a2e0c8291
amin00c1309 <37907284+amin00c1309@users.noreply.github.com>
2020-01-15 06:48:43 -0800

    file download security

--- projects/front/libraryfront/src/app/app.module.ts
+++ projects/front/libraryfront/src/app/app.module.ts
@@ -5,19 +5,32 @@ import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { DataTablesModule } from 'angular-datatables';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatDialogModule, MatProgressSpinnerModule, MatButtonModule } from '@angular/material';
+import { ConfirmationPopoverModule } from 'angular-confirmation-popover';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+
   ],
   imports: [
     BrowserModule,
     AgGridModule.withComponents([]),
     BrowserAnimationsModule,
-    DataTablesModule
+    DataTablesModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    ConfirmationPopoverModule.forRoot({
+      confirmButtonType: 'success', cancelButtonType: 'danger', confirmText: 'Təsdiq', cancelText: 'Ləğv'
+    })
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
